2a158a92055e1f006777f30128adef14dd785347
Manish Luthyagi <manishluthyagi@Manishs-MacBook-Pro.local>
2020-02-18 16:50:51 +0530

    manish: changed code now searching peak value in starting two elements

--- local_peak.py
+++ local_peak.py
@@ -4,16 +4,24 @@ import unittest
 def local(array_list):
     peak = []
     if len(array_list) != 0:
-        peak.append(array_list[-1])
+        if array_list[0] > array_list[1]:
+            peak.append(array_list[0])
+        else:
+            peak.append(array_list[1])
+
     return peak
 
 
 class LocalPeak(unittest.TestCase):
     def test_for_local_peak_in_empty_list(self):
-        self.assertEqual(local([]), [])
+        self.assertEqual([], local([]))
 
     def test_for_first_local_peak_in_sorted_list_ascending_order(self):
-        self.assertEqual(local([0, 1, 2, 3]), [3])
+        self.assertEqual([1], local([0, 1, 2, 3, 4]))
+
+    def test_for_first_local_peak_in_sorted_list_descending_order(self):
+        self.assertEqual([4], local([4, 3, 2, 1, 0]))
+
 
 
 if __name__ == '__main__':
