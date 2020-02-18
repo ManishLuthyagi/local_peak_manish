fb82b6e3532ec463e5c99b4bd391240128fd1907
Manish Luthyagi <manishluthyagi@Manishs-MacBook-Pro.local>
2020-02-18 16:35:04 +0530

    manish: checked for non empty sorted list in ascending order

--- local_peak.py
+++ local_peak.py
@@ -2,12 +2,18 @@ import unittest
 
 
 def local(array_list):
+
+    if len(array_list) != 0:
+        return [array_list[-1]]
     return []
 
 
 class LocalPeak(unittest.TestCase):
     def test_for_local_peak_in_empty_list(self):
         self.assertEqual(local([]), [])
+
+    def test_for_first_local_peak_in_sorted_list_ascending_order(self):
+        self.assertEqual(local([0, 1, 2, 3]), [3])
 
 
 if __name__ == '__main__':
